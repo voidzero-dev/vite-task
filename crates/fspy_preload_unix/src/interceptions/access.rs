415128eb4fad9d26ec061ff199382293ccaa8303
branchseer <dk4rest@gmail.com>
2025-11-18 22:45:13 +0800

    intercept access/faccessat on unix

---
+++ crates/fspy_preload_unix/src/interceptions/access.rs
@@ -0,0 +1,28 @@
+use fspy_shared::ipc::AccessMode;
+use libc::{c_char, c_int};
+
+use crate::{
+    client::{convert::PathAt, handle_open},
+    macros::intercept,
+};
+
+intercept!(access(64): unsafe extern "C" fn(pathname: *const c_char, mode: c_int) -> c_int);
+unsafe extern "C" fn access(pathname: *const c_char, mode: c_int) -> c_int {
+    unsafe {
+        handle_open(pathname, AccessMode::Read);
+    }
+    unsafe { access::original()(pathname, mode) }
+}
+
+intercept!(faccessat(64): unsafe extern "C" fn(dirfd: c_int, pathname: *const c_char, mode: c_int, flags: c_int) -> c_int);
+unsafe extern "C" fn faccessat(
+    dirfd: c_int,
+    pathname: *const c_char,
+    mode: c_int,
+    flags: c_int,
+) -> c_int {
+    unsafe {
+        handle_open(PathAt(dirfd, pathname), AccessMode::Read);
+    }
+    unsafe { faccessat::original()(dirfd, pathname, mode, flags) }
+}
--- crates/fspy_preload_unix/src/interceptions/mod.rs
+++ crates/fspy_preload_unix/src/interceptions/mod.rs
@@ -1,3 +1,4 @@
+mod access;
 mod dirent;
 mod open;
 mod spawn;
