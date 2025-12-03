6c6ceaf81a8f4d34ab098b97951825330bcd46a2
branchseer <dk4rest@gmail.com>
2025-12-03 23:13:50 +0800

    add tests

--- crates/vite_task_graph/src/config.rs
+++ crates/vite_task_graph/src/config.rs
@@ -6,8 +6,8 @@ use vite_path::RelativePathBuf;
 use vite_str::Str;
 
 /// Cache-related fields of a task defined by user in `vite.config.*`
-#[derive(Debug, Deserialize)]
-#[serde(untagged, rename_all = "camelCase")]
+#[derive(Debug, Deserialize, PartialEq, Eq)]
+#[serde(untagged, deny_unknown_fields, rename_all = "camelCase")]
 pub enum UserCacheConfig {
     /// Cache is enabled
     Enabled {
@@ -17,9 +17,11 @@ pub enum UserCacheConfig {
 
         // Fields only relevant when cache is enabled
         /// Environment variable names to be fingerprinted and passed to the task.
+        #[serde(default)] // default to empty if omitted
         envs: HashSet<Str>,
 
         /// Environment variable names to be passed to the task without fingerprinting.
+        #[serde(default)] // default to empty if omitted
         pass_through_envs: HashSet<Str>,
     },
     /// Cache is disabled
@@ -30,8 +32,8 @@ pub enum UserCacheConfig {
 }
 
 /// Task configuration defined by user in `vite.config.*`
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Deserialize, PartialEq, Eq)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct UserTaskConfig {
     /// If None, the script from `package.json` with the same name will be used
     command: Option<Box<str>>,
@@ -52,4 +54,64 @@ pub struct UserTaskConfig {
 /// User configuration file structure for `vite.config.*`
 pub struct UserConfigFile {
     tasks: HashMap<Str, UserTaskConfig>,
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn test_defaults() {
+        let user_config_json = json!({});
+        let user_config: UserTaskConfig = serde_json::from_value(user_config_json).unwrap();
+        assert_eq!(
+            user_config,
+            UserTaskConfig {
+                command: None,
+                cwd: "".try_into().unwrap(),
+                depends_on: HashSet::new(),
+                cache_config: UserCacheConfig::Enabled {
+                    cache: MustBe!(true),
+                    envs: HashSet::new(),
+                    pass_through_envs: HashSet::new(),
+                },
+            }
+        );
+    }
+
+    #[test]
+    fn test_cache_disabled() {
+        let user_config_json = json!({
+            "cache": false
+        });
+        let user_config: UserTaskConfig = serde_json::from_value(user_config_json).unwrap();
+        assert_eq!(user_config.cache_config, UserCacheConfig::Disabled { cache: MustBe!(false) });
+    }
+
+    #[test]
+    fn test_cache_explictly_enabled() {
+        let user_config_json = json!({
+            "cache": true,
+            "envs": ["NODE_ENV"],
+        });
+        assert_eq!(
+            serde_json::from_value::<UserCacheConfig>(user_config_json).unwrap(),
+            UserCacheConfig::Enabled {
+                cache: MustBe!(true),
+                envs: ["NODE_ENV".into()].into_iter().collect(),
+                pass_through_envs: HashSet::new(),
+            },
+        );
+    }
+
+    #[test]
+    fn test_cache_disabled_but_with_fields() {
+        let user_config_json = json!({
+            "cache": false,
+            "envs": ["NODE_ENV"],
+        });
+        assert!(serde_json::from_value::<UserCacheConfig>(user_config_json).is_err());
+    }
 }
