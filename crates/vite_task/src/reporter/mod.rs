352b7246a4583dc310f97937d3766679fa5398a1
branchseer <dk4rest@gmail.com>
2025-12-05 12:25:26 +0800

    remove unnecessary files

--- crates/vite_task/src/reporter/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod stream;
-
-/// Describes how to report events during a Vite Task session.
-/// It's an abstraction over different kinds of ui (stream, terminial ui, web, etc).
-pub trait Reporter {
-    /// Report the execution plan that is about to be executed.
-    fn report_execution_plan(self: Box<Self>, tree: &str);
-}
--- crates/vite_task/src/reporter/stream.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Default)]
-pub struct StreamReporter(());
-
-impl crate::reporter::Reporter for StreamReporter {
-    fn report_execution_plan(self: Box<Self>, tree: &str) {}
-}
