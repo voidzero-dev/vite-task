b9661b9d140b46394972571a13b8ae96983cf421
branchseer <dk4rest@gmail.com>
2025-12-07 07:35:26 +0800

    init execution graph snapshot testing

--- crates/vite_task_graph/tests/snapshots.rs
+++ crates/vite_task_graph/tests/snapshots.rs
@@ -1,11 +1,39 @@
-use std::path::Path;
+use core::panic;
+use std::{path::Path, sync::Arc};
 
+use clap::Parser;
 use copy_dir::copy_dir;
 use tokio::runtime::Runtime;
 use vite_path::{AbsolutePath, RelativePathBuf};
 use vite_str::Str;
-use vite_task_graph::{IndexedTaskGraph, TaskDependencyType, TaskId, loader::JsonUserConfigLoader};
+use vite_task_graph::{
+    IndexedTaskGraph, TaskDependencyType, TaskId,
+    loader::JsonUserConfigLoader,
+    query::{TaskExecutionGraph, cli::CLITaskQuery},
+};
 use vite_workspace::find_workspace_root;
+
+#[derive(serde::Serialize, PartialEq, PartialOrd, Eq, Ord)]
+struct TaskIdSnapshot {
+    package_dir: RelativePathBuf,
+    task_name: Str,
+}
+impl TaskIdSnapshot {
+    fn from_task_id(
+        task_id: &TaskId,
+        base_dir: &AbsolutePath,
+        indexed_task_graph: &IndexedTaskGraph,
+    ) -> Self {
+        Self {
+            task_name: task_id.task_name.clone(),
+            package_dir: indexed_task_graph
+                .get_package_path(task_id.package_index)
+                .strip_prefix(base_dir)
+                .unwrap()
+                .unwrap(),
+        }
+    }
+}
 
 /// Create a stable json representation of the task graph for snapshot testing.
 ///
@@ -14,28 +42,6 @@ fn snapshot_task_graph(
     indexed_task_graph: &IndexedTaskGraph,
     base_dir: &AbsolutePath,
 ) -> impl serde::Serialize {
-    #[derive(serde::Serialize, PartialEq, PartialOrd, Eq, Ord)]
-    struct TaskIdSnapshot {
-        package_dir: RelativePathBuf,
-        task_name: Str,
-    }
-    impl TaskIdSnapshot {
-        fn from_task_id(
-            task_id: &TaskId,
-            base_dir: &AbsolutePath,
-            indexed_task_graph: &IndexedTaskGraph,
-        ) -> Self {
-            Self {
-                task_name: task_id.task_name.clone(),
-                package_dir: indexed_task_graph
-                    .get_package_path(task_id.package_index)
-                    .strip_prefix(base_dir)
-                    .unwrap()
-                    .unwrap(),
-            }
-        }
-    }
-
     #[derive(serde::Serialize)]
     struct TaskNodeSnapshot {
         id: TaskIdSnapshot,
@@ -76,6 +82,52 @@ fn snapshot_task_graph(
     node_snapshots
 }
 
+/// Create a stable json representation of the task graph for snapshot testing.
+///
+/// All paths are relative to `base_dir`.
+fn snapshot_execution_graph(
+    execution_graph: &TaskExecutionGraph,
+    indexed_task_graph: &IndexedTaskGraph,
+    base_dir: &AbsolutePath,
+) -> impl serde::Serialize {
+    #[derive(serde::Serialize, PartialEq, PartialOrd, Eq, Ord)]
+    struct EdgeSnapshot {
+        from: TaskIdSnapshot,
+        to: TaskIdSnapshot,
+    }
+    let task_graph = indexed_task_graph.task_graph();
+    let mut edge_snapshots = Vec::<EdgeSnapshot>::new();
+    for (from_index, to_index, ()) in execution_graph.all_edges() {
+        edge_snapshots.push(EdgeSnapshot {
+            from: TaskIdSnapshot::from_task_id(
+                &task_graph[from_index].task_id,
+                base_dir,
+                indexed_task_graph,
+            ),
+            to: TaskIdSnapshot::from_task_id(
+                &task_graph[to_index].task_id,
+                base_dir,
+                indexed_task_graph,
+            ),
+        });
+    }
+    edge_snapshots.sort_unstable();
+    edge_snapshots
+}
+
+#[derive(serde::Deserialize)]
+struct CLIQuery {
+    pub name: Str,
+    pub args: Vec<Str>,
+    pub cwd: RelativePathBuf,
+}
+
+#[derive(serde::Deserialize, Default)]
+struct CLIQueriesFile {
+    #[serde(rename = "query")] // toml usually uses singular for arrays
+    pub queries: Vec<CLIQuery>,
+}
+
 fn run_case(runtime: &Runtime, tmpdir: &AbsolutePath, case_path: &Path) {
     let case_name = case_path.file_name().unwrap().to_str().unwrap();
     if case_name.starts_with(".") {
@@ -94,6 +146,13 @@ fn run_case(runtime: &Runtime, tmpdir: &AbsolutePath, case_path: &Path) {
         case_name
     );
 
+    let cli_queries_toml_path = case_path.join("cli-queries.toml");
+    let cli_queries_file: CLIQueriesFile = match std::fs::read(&cli_queries_toml_path) {
+        Ok(content) => toml::from_slice(&content).unwrap(),
+        Err(err) if err.kind() == std::io::ErrorKind::NotFound => Default::default(),
+        Err(err) => panic!("Failed to read cli-queries.toml for case {}: {}", case_name, err),
+    };
+
     runtime.block_on(async {
         let indexed_task_graph = vite_task_graph::IndexedTaskGraph::load(
             workspace_root,
@@ -102,6 +161,37 @@ fn run_case(runtime: &Runtime, tmpdir: &AbsolutePath, case_path: &Path) {
         .await
         .expect(&format!("Failed to load task graph for case {case_name}"));
         let task_graph_snapshot = snapshot_task_graph(&indexed_task_graph, &case_stage_path);
+
+        for cli_query in cli_queries_file.queries {
+            let cli_task_query = CLITaskQuery::try_parse_from(
+                std::iter::once("vite-run") // dummy program name
+                    .chain(cli_query.args.iter().map(|s| s.as_str())),
+            )
+            .expect(&format!(
+                "Failed to parse CLI args for query '{}' in case '{}'",
+                cli_query.name, case_name
+            ));
+
+            let cwd: Arc<AbsolutePath> = case_stage_path.join(&cli_query.cwd).into();
+            let task_query = cli_task_query.into_task_query(&cwd).expect(&format!(
+                "Invalid task query for query '{}' in case '{}'",
+                cli_query.name, case_name
+            ));
+
+            let execution_graph = indexed_task_graph.query_tasks(task_query).expect(&format!(
+                "Failed to query tasks for query '{}' in case '{}'",
+                cli_query.name, case_name
+            ));
+
+            let task_graph_snapshot =
+                snapshot_execution_graph(&execution_graph, &indexed_task_graph, &case_stage_path);
+
+            insta::assert_json_snapshot!(
+                format!("execution graph - {}", cli_query.name),
+                task_graph_snapshot
+            );
+        }
+
         insta::assert_json_snapshot!("task graph", task_graph_snapshot);
     });
 }
