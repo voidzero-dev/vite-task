98df43d750b607e86e686aeb6ca47cc5fd25b89a
branchseer <dk4rest@gmail.com>
2026-01-31 21:53:02 +0800

    add is_terminal test

--- crates/vite_pty/tests/terminal.rs
+++ crates/vite_pty/tests/terminal.rs
@@ -1,8 +1,16 @@
-use std::io::{IsTerminal, stdin};
+use std::io::{IsTerminal, stderr, stdin, stdout};
 
+use portable_pty::CommandBuilder;
 use subprocess_test::command_for_fn;
+use vite_pty::{geo::ScreenSize, terminal::Terminal};
 
 #[test]
 fn is_terminal() {
-    command_for_fn!((), |_: ()| { println!("{}", stdin().is_terminal()) });
+    let cmd = CommandBuilder::from(command_for_fn!((), |_: ()| {
+        println!("{} {} {}", stdin().is_terminal(), stdout().is_terminal(), stderr().is_terminal())
+    }));
+
+    let mut terminal = Terminal::spawn(ScreenSize { rows: 80, cols: 80 }, cmd).unwrap();
+    let output = terminal.read_to_end().unwrap();
+    assert_eq!(output.trim(), "true true true");
 }
