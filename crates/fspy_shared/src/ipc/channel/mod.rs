f13c2f6f3cd2b37f2527fa658e55f536c973b2f0
Ludea <kudealol.olol@gmail.com>
2026-04-10 09:13:27 +0200

    missing type from shared_memory crate

--- crates/fspy_shared/src/ipc/channel/mod.rs
+++ crates/fspy_shared/src/ipc/channel/mod.rs
@@ -3,6 +3,8 @@
 use std::{env::temp_dir, fs::File, io, ops::Deref, path::PathBuf, sync::Arc};
 
 use bincode::{Decode, Encode};
+#[cfg(not(target_os = "android"))]
+use shared_memory::{Shmem, ShmemConf};
 use shm_io::{ShmReader, ShmWriter};
 use tracing::debug;
 use uuid::Uuid;
